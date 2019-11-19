94ad28416e348f5662a3f26056ce93ca9abd45c4
Compl Yue <compl.yue@icloud.com>
2019-11-20 02:54:16 +0800

    switch to Nix

---
+++ shell.nix
@@ -0,0 +1,15 @@
+# this defines the Nix env to run this interactive Haskell project
+with (import (
+# to use a version of Haze from github
+  # builtins.fetchTarball {
+  #   url = "https://github.com/complyue/haze/archive/0.1.0.rc0.tar.gz";
+  #   sha256 = "xxx";
+  # }
+
+  # to use the version of Haze checked out locally
+  ../haze
+) { });
+haskellPackages.shellFor {
+  packages = p: [ p.zlib p.haze ];
+  nativeBuildInputs = [ pkgs.stack pkgs.hadui ];
+}
