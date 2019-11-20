6de3c1b10c8d3666b3b2281f13f4eb62aac88b8e
ComplYue <compl.yue@icloud.com>
2019-11-20 17:23:17 +0800

    upd

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
 # this defines the Nix env to run this interactive Haskell project
 with (import (
 # to use a version of Haze from github
-  # builtins.fetchTarball {
-  #   url = "https://github.com/complyue/haze/archive/0.1.0.rc0.tar.gz";
-  #   sha256 = "xxx";
-  # }
+#   builtins.fetchTarball {
+#     url = "https://github.com/complyue/haze/archive/0.1.0.0.tar.gz";
+#     sha256 = "xxx";
+#   }
 
   # to use the version of Haze checked out locally
   ../haze
